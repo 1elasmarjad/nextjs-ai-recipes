22148725f05a4de25e04e11ac03525c1e9d86964
1elasmarjad <jadelasmar4@gmail.com>
2024-06-14 18:20:49 -0400

    🥕 Worked on landing page use effect for autocomplete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link";
 import SearchAutocomplete from "~/components/autocomplete";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen w-screen">
       <section className="flex min-h-screen flex-col items-center justify-center">
-        <div className="flex flex-col">
-          <div className="flex max-w-lg flex-col items-center gap-6">
+        <div className="flex flex-col w-full items-center">
+          <div className="flex max-w-lg w-full flex-col items-center gap-6">
             <h1 className="text-5xl">Lets Cook.</h1>
             <SearchAutocomplete/>
           </div>
--- src/components/autocomplete.tsx
+++ src/components/autocomplete.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
 import { CircularProgress } from "@mui/material";
 
 interface Ingredient {
@@ -13,42 +12,41 @@ interface Ingredient {
 export default function SearchAutocomplete() {
   const [options, setOptions] = React.useState<readonly Ingredient[]>([]);
   const [searchValue, setSearchValue] = React.useState<string>("");
-
-  const isLoading = false;
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   React.useEffect(() => {
-    setOptions([
-      { name: "Tomato" },
-    ]);
-  }, []);
+    setIsLoading(true);
+    setOptions([{ name: "Tomato" }]);
+    setIsLoading(false);
+  }, [searchValue]);
 
   return (
-    <Stack spacing={3} sx={{ width: 500 }}>
-      <Autocomplete
-        multiple
-        id="tags-outlined"
-        options={options}
-        getOptionLabel={(option) => option.name}
-        defaultValue={[]}
-        filterSelectedOptions
-        renderInput={(params) => (
-          <TextField
+    <Autocomplete
+      multiple
+      className="w-full"
+      id="tags-outlined"
+      options={options}
+      getOptionLabel={(option) => option.name}
+      defaultValue={[]}
+      filterSelectedOptions
+      renderInput={(params) => (
+        <TextField
           {...params}
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
-          label="Asynchronous"
+          label="Ingredients"
           InputProps={{
             ...params.InputProps,
             endAdornment: (
-              <React.Fragment>
-                {isLoading ? <CircularProgress color="inherit" size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </React.Fragment>
+              <>
+                {isLoading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+              </>
             ),
           }}
         />
-        )}
-      />
-    </Stack>
+      )}
+    />
   );
 }
